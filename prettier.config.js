dfe755664917c02bad80d4eaefa78504a7e6e697
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-02-14 20:35:04 -0800

    style: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    tailwindConfig: './tailwind.config.js',
+    tabWidth: 4
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  theme: {
-    colors: {
-      primary: "#11161E",
-      secondary: "#282C2F",
-      ternary: "#48FFFF",
-      accent: "#E6E3DF",
-    }
-  },
-  content: ["./index.html", "./script/**.js"],
-  plugins: [require('tailwind-scrollbar')]
-}
+    theme: {
+        colors: {
+            primary: "#11161E",
+            secondary: "#282C2F",
+            neutral: "#E6E3DF",
+            accent: "#9adc58",
+            selection: "#3498db",
+        },
+    },
+    content: ["./index.html", "./script/**.js"],
+    plugins: [
+        require("tailwind-scrollbar"),
+        require("prettier-plugin-tailwindcss"),
+    ],
+};
