c0eb608670c59c0566b0ad85b688a07b3d1e0306
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-03-23 12:16:20 -0700

    Refactor downloader to properly handle async and wrapped types

--- generator/downloader.py
+++ generator/downloader.py
@@ -1,75 +1,84 @@
 import asyncio
-import functools
-from itertools import product
-from enum import Enum, StrEnum
-from string import Template
+from http.client import HTTPException
 
 from httpx import AsyncClient
 
-from generator.types import DownloadedRepoData, RawData, RepoData
+from generator.parsers.parser_type import ParserType
+from generator.source_type import SourceType
+from generator.types import RepoData, RepoMeta, WrappedRepoData
 
 
-class SourceType(StrEnum):
-    JAVA_SOURCE = 'java'
-    KOTLIN_SOURCE = 'kotlin'
-    LANG_FILE = 'resources/assets'
-
-
-class RemoteHostTemplate(Enum):
-    GITHUB = Template(
-        'https://raw.githubusercontent.com/$repo/$branch/src/main/$source_path/$file_path'
-    )
-    GITLAB = Template(
-        'https://gitlab.com/$repo/-/raw/$branch/src/main/$source_path/$file_path'
-    )
-
-    @classmethod
-    def build_url(cls, *, remote: str, repo: str, branch: str, source_path: SourceType, file_path: str) -> str:
-        result = cls.__members__.get(remote.upper(), None)
-
-        if result is None:
-            raise ValueError(f"Unsupported host '{remote}' used by '{repo}/{branch}'")
-
-        return result.value.substitute(
-            repo=repo,
+async def download_file(
+    client: AsyncClient,
+    repo: RepoData,
+    branch: str,
+    source_path: SourceType,
+    file_path: str,
+) -> str:
+    response = await client.get(
+        repo.source.build_url(
+            repo=repo.owner_repo,
             branch=branch,
             source_path=source_path,
             file_path=file_path,
         )
-
-
-async def download_repo(client: AsyncClient, parser: str, repo: RepoData) -> DownloadedRepoData:
-    settings_url_partial = functools.partial(
-        RemoteHostTemplate.build_url,
-        remote='github' if repo.source is None else repo.source,
-        repo=repo.owner_repo,
-        source_path=SourceType.JAVA_SOURCE,
     )
 
-    for branch, setting_file in product(repo.branches, repo.settings_file_paths):
-        url = settings_url_partial(branch=branch, file_path=setting_file)
-        contents = await client.get(url)
+    if response.status_code != 200:
+        raise HTTPException(f'Failed to fetch {response.url}')
 
-    lang_file_url_partial = functools.partial(
-        RemoteHostTemplate.build_url,
-        remote='github' if repo.source is None else repo.source,
-        repo=repo.owner_repo,
-        source_path=SourceType.JAVA_SOURCE,
-        file_path=repo.lang_file_path
-    )
+    return response.text
 
+
+async def download_repo(
+    client: AsyncClient, parser: str, repo: RepoData
+) -> list[WrappedRepoData]:
+
+    branches: list[WrappedRepoData] = []
     for branch in repo.branches:
-        url = lang_file_url_partial(branch=branch)
-        contents = await client.get(url)
+        async with asyncio.TaskGroup() as tg:
+            settings_file_tasks = [
+                tg.create_task(
+                    download_file(
+                        client,
+                        repo,
+                        branch,
+                        SourceType.JAVA_SOURCE,
+                        settings_file,
+                    )
+                )
+                for settings_file in repo.settings_file_paths
+            ]
 
-    raise NotImplemented()
+        settings_files = [task.result() for task in settings_file_tasks]
+
+        if not parser == ParserType.LEGACY:
+            raw_lang = await download_file(
+                client, repo, branch, SourceType.LANG_FILE, repo.lang_file_path
+            )
+        else:
+            raw_lang = None
+
+        branches.append(
+            WrappedRepoData(
+                name=repo.name,
+                owner_repo=repo.owner_repo,
+                branch=branch,
+                raw_settings_files=settings_files,
+                raw_lang_file=raw_lang,
+                source=repo.source,
+                parser=parser,
+            )
+        )
+
+    return branches
 
 
-async def fetch_data(repos: RawData):
+async def fetch_data(repos: RepoMeta) -> list[WrappedRepoData]:
     async with AsyncClient() as client:
         tasks = [
             download_repo(client, parser, repo)
             for parser, repos in repos.combined()
             for repo in repos
         ]
-        return asyncio.gather(*tasks, return_exceptions=True)
+        return await asyncio.gather(*tasks, return_exceptions=False)
