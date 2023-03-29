926ac4a10f8e05ffe4c6be902bc358247f42e617
Crec0 <83436716+Crec0@users.noreply.github.com>
2023-03-28 17:46:46 -0700

    Async parser code

--- generator/main.py
+++ generator/main.py
@@ -4,57 +4,37 @@ import json
 import pytoml
 
 from generator.downloader import fetch_data
-from generator.types import RepoMeta, WrappedDownloadedData
+from generator.parsers.abstract_parser import AbstractParser
+from generator.parsers.parser_type import ParserType
+from generator.parsers.v1_parser import V1Parser
+from generator.parsers.v2_parser import V2Parser
+from generator.parsers.v2_yaml_parser import V2YamlParser
+from generator.tokenizer.rule import Rule, group_by_repo
+from generator.types import RepoMeta, WrappedDownloadedData, WrappedRepoData
+from generator.util import webhook_stats
 
 
-# async def parse_rules_for_version(
-#     version: ParserType,
-#     source_dict: dict[str, str],
-#     lang_dict: Optional[dict[str, str]] = None,
-# ) -> list[Rule]:
-#     if lang_dict is None:
-#         lang_dict = defaultdict(lambda: None)
-#
-#     parsed_rules: list[Rule] = []
-#
-#     for source_path, source in source_dict.items():
-#
-#         parser: AbstractParser
-#
-#         if version == ParserType.LEGACY:
-#             parser = V1Parser(source_path, source, lang_dict[source_path])
-#         elif version == ParserType.TRANSLATIONS_JSON:
-#             parser = V2Parser(source_path, source, lang_dict[source_path])
-#         elif version == ParserType.TRANSLATIONS_YAML:
-#             print('V2YAML')
-#             continue
-#             # parser = V2YamlParser(source_path, source, lang_dict[source_path])
-#         else:
-#             raise ValueError(f'Unknown parser version: {version}')
-#
-#         parser.parse()
-#         parsed_rules.extend(parser.rules)
-#
-#     return parsed_rules
-#
-#
-# async def process_data(data_json) -> list[Rule]:
-#     return functools.reduce(
-#         operator.iconcat,
-#         await asyncio.gather(
-#             *[
-#                 parse_rules_for_version(
-#                     version,
-#                     source_lang_dict['source'],
-#                     source_lang_dict['lang']
-#                     if 'lang' in source_lang_dict
-#                     else None,
-#                 )
-#                 for (version, source_lang_dict) in data_json.items()
-#             ]
-#         ),
-#         [],
-#     )
+def parser_type_by_name(parser_type: ParserType) -> Type[AbstractParser]:
+    match parser_type:
+        case ParserType.LEGACY:
+            return V1Parser
+        case ParserType.TRANSLATIONS_JSON:
+            return V2Parser
+        case ParserType.TRANSLATIONS_YAML:
+            return V2YamlParser
+
+
+async def process_repo(repo: WrappedRepoData) -> list[Rule]:
+    parser = parser_type_by_name(repo.parser)(repo)
+    parser.parse()
+    return parser.rules
+
+
+async def process_data(data: WrappedDownloadedData) -> list[Rule]:
+    async with asyncio.TaskGroup() as tg:
+        tasks = [tg.create_task(process_repo(repo)) for repo in data.repos]
+    rules = [task.result() for task in tasks]
+    return reduce(operator.iconcat, rules, [])
 
 
 async def generate():
