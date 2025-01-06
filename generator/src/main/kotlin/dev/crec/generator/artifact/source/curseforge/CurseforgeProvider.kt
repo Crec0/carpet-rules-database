6aeed2145c029d208b93e67fcd92033b0906c716
Crec0 <83436716+Crec0@users.noreply.github.com>
2025-01-05 17:05:51 -0800

    wip

--- generator/src/main/kotlin/dev/crec/generator/artifact/source/curseforge/CurseforgeProvider.kt
+++ generator/src/main/kotlin/dev/crec/generator/artifact/source/curseforge/CurseforgeProvider.kt
@@ -9,6 +9,7 @@ import dev.crec.generator.getRequest
 import dev.crec.generator.mapConcurrently
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
+import java.util.zip.ZipInputStream
 
 
 class CurseforgeProvider(client: OkHttpClient, moshi: Moshi) : AbstractProvider(client, moshi) {
@@ -17,7 +18,7 @@ class CurseforgeProvider(client: OkHttpClient, moshi: Moshi) : AbstractProvider(
     private val curseAdapter = moshi.adapter<FilesResponseSchema>()
 
     private fun filterCachedVersions(projectId: String, metadataList: List<ModFileMetadata>): List<ModFileMetadata> {
-        return metadataList.filterNot { cacheManager.isCached(projectId, it.id) }
+        return metadataList.filterNot { cacheManager.isCached(projectId, it.id) }.take(1)
     }
 
     override suspend fun process(projectId: String) {
@@ -27,16 +28,27 @@ class CurseforgeProvider(client: OkHttpClient, moshi: Moshi) : AbstractProvider(
 
         val uncachedVersions = filterCachedVersions(projectId, metadata.data)
 
-        uncachedVersions.forEach { meta ->
+
+        uncachedVersions.mapConcurrently(5) { meta ->
             val cdnReq = meta.toCDN().getRequest()
-//            val bytes = client.execute(cdnReq).use { it.body.bytes() }
-//            processVersion(projectId, meta.id.toString(), bytes)
+            val bytes = client.execute(cdnReq).use { it.body.bytes() }
+
+            ZipInputStream(bytes.inputStream()).use { it ->
+                var entry = it.nextEntry
+                while (entry != null) {
+                    println(entry.name)
+                    entry = it.nextEntry
+                }
+            }
+
+            processVersion(projectId, meta.id.toString(), bytes)
+
             cacheManager.addToCache(projectId, meta.id)
         }
     }
 
     override suspend fun processVersion(projectId: String, fileId: String, bytes: ByteArray) {
-        TODO("Not yet implemented")
+        println("Processing $fileId")
     }
 
     companion object {
