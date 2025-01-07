e3bda3a4e9717859bbdde0a4a94de308470904a5
Crec0 <83436716+Crec0@users.noreply.github.com>
2025-01-06 22:26:28 -0800

    Add title when sidebar is collapsed and add max rules per page

--- frontend/src/routes/(components)/SiteTitle.svelte
+++ frontend/src/routes/(components)/SiteTitle.svelte
@@ -1,8 +1,16 @@
-<script>
+<script lang="ts">
     import { Database } from "lucide-svelte";
+
+    interface Props {
+        background?: string;
+    }
+
+    let {
+        background = ""
+    }: Props = $props();
 </script>
 
-<div class="sticky top-0 flex items-center px-4 py-2 text-2xl space-x-2">
+<div class="sticky top-0 flex items-center px-4 py-2 text-2xl space-x-2 {background}">
     <span>Carpet Rules</span>
     <Database class="h-6 w-6" />
 </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -14,31 +14,41 @@
     import { SidebarTrigger, useSidebar } from "$lib/components/ui/sidebar";
     import { cn } from "$lib/utils";
     import { gState } from "$lib/app-state.svelte";
+    import SiteTitle from "./(components)/SiteTitle.svelte";
+    import { Select, SelectContent, SelectItem, SelectTrigger } from "$lib/components/ui/select";
 
+
+    const availablePerPage = [ "10", "25", "50", "75", "100" ];
 
     let currentPage = $state(1);
+    let selectedPerPage = $state("50");
 
-    const PER_PAGE = 50;
     const sidebar = useSidebar();
-    const pageStart = $derived(( currentPage - 1 ) * PER_PAGE);
-    const pageEnd = $derived(pageStart + PER_PAGE);
+
+    const perPage = $derived(Number(selectedPerPage));
+    const pageStart = $derived(( currentPage - 1 ) * perPage);
+    const pageEnd = $derived(pageStart + perPage);
 </script>
 
-<div class="sticky top-0 flex items-center border-b p-2 shadow-sm bg-background space-x-2">
+{#if !sidebar.open}
+    <SiteTitle background="bg-background" />
+{/if}
+
+<div
+    class={cn("sticky flex items-center border-b p-2 shadow-sm bg-background space-x-2", sidebar.open ? "top-0" : "top-12")}>
     <SidebarTrigger />
     <ThemeSwitcher />
 </div>
 
 <div class="mb-14 grid grid-cols-2 gap-4 p-4">
     {#each gState.filteredRules.slice(pageStart, pageEnd) as rule}
-        <!--{#each parsedRules.filter(r => r.name === "accurateAzaleaLeafDistribution" || r.name === "hopperUpdateFix" || r.name === "reachDistance").slice(pageStart, pageEnd) as rule}-->
         <Rule {rule} />
     {/each}
 </div>
 
 <div
-    class={cn("fixed bottom-0 w-full border-t p-2 shadow-sm duration-200 ease-linear bg-background contents-center transition-[width]", sidebar.open ? "w-[calc(100%-var(--sidebar-width))]" : "")}>
-    <Pagination bind:page={currentPage} class="w-max" count={gState.rulesCount} perPage={PER_PAGE}>
+    class={cn("fixed bottom-0 flex w-full justify-center border-t p-2 shadow-sm duration-200 ease-linear space-x-4 bg-background contents-center transition-[width]", sidebar.open ? "w-[calc(100%-var(--sidebar-width))]" : "")}>
+    <Pagination bind:page={currentPage} class="mx-0 w-max" count={gState.rulesCount} perPage={perPage}>
         {#snippet children({ pages, currentPage })}
             <PaginationContent>
                 <PaginationItem>
@@ -63,4 +73,17 @@
             </PaginationContent>
         {/snippet}
     </Pagination>
+    <div class="w-max">
+        <Select bind:value={selectedPerPage} type="single">
+            <SelectTrigger class="space-x-2">
+                <span class="text-sm">Per Page</span>
+                <span class="text-sm">{selectedPerPage}</span>
+            </SelectTrigger>
+            <SelectContent>
+                {#each availablePerPage as perPage}
+                    <SelectItem value={perPage}>{perPage}</SelectItem>
+                {/each}
+            </SelectContent>
+        </Select>
+    </div>
 </div>
